7835306218f3d65434203c024af4a4c61e81740e
Saif Mahamood <saifmahamood@v1020-wn-78-185.campus-dynamic.uwaterloo.ca>
2017-03-02 02:52:27 -0500

    voting page hardcoded

---
+++ votingResults.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+import {
+  AppRegistry,
+  StyleSheet,
+  Navigator,
+  Text,
+  View,
+  Button,
+  TouchableHighlight
+} from 'react-native';
+
+export default class LandingPage extends Component {
+
+  render() {
+    return (
+      <View>
+        <Text style={styles.welcome}>
+         You have Killed Duaa!
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  welcome: {
+    fontSize: 40,
+    textAlign: 'center',
+    marginTop: 160,
+    marginBottom: 40,
+    fontWeight: "100",
+  },
+});
