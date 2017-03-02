f8ba876599de7a23fe085fbe3b93ed099dc3f84b
sheethala <s2swamin@uwaterloo.ca>
2017-03-02 15:45:15 -0500

    Added props

--- votingPage.js
+++ votingPage.js
@@ -53,9 +53,10 @@ export default class VotingPage extends Component {
 
     completeVote(){
       console.log('yo');
+			console.log(this.props.gameId);
 			firebase.database().ref('Players/' + this.props.gameId  + '-players/2').update({
 				'status': 0
-			})
+			});
       this.props.navigator.push({
         id: 'VotingResults'
       })
