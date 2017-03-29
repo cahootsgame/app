21a8efd094d4a15fd44dcac31247885cea94bdc1
Rithu Chandrasekar <crithu@gmail.com>
2017-03-29 05:38:33 -0400

    Opening messenger works

--- landingPage.js
+++ landingPage.js
@@ -16,10 +16,39 @@ import {
   Text,
   View,
   Button,
-  TouchableHighlight
+  TouchableHighlight,
+  TouchableOpacity,
+  Linking
 } from 'react-native';
 
 // Can we assign player IDs here?
+
+class OpenURLButton extends React.Component {
+  static propTypes = {
+    url: React.PropTypes.string,
+  };
+
+  handleClick = () => {
+    Linking.canOpenURL(this.props.url).then(supported => {
+      if (supported) {
+        Linking.openURL(this.props.url);
+      } else {
+        console.log('Don\'t know how to open URI: ' + this.props.url);
+      }
+    });
+  };
+
+  render() {
+    return (
+      <TouchableOpacity
+        onPress={this.handleClick}>
+        <View style={styles.button}>
+          <Text style={styles.text}>Open {this.props.url}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
 
 
 export default class LandingPage extends Component {
@@ -33,6 +62,8 @@ export default class LandingPage extends Component {
         <TouchableButton  onButtonClick={this.onPressStartNewGame.bind(this)} text={"START"}/>
         <TouchableButton onButtonClick={this.onPressConnectGame.bind(this)} text={"CONNECT TO AN EXISTING GAME"}/>
         <TouchableButton text={"HOW TO PLAY"}/>
+        <OpenURLButton url={'fb://notifications'} />
+        <OpenURLButton url={'fb-messenger://app'} />
       </View>
     );
   }
