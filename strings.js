a8ac47f13c01007ce9d6e06e9e365f4e70f48c67
Saif Mahamood <saifmahamood@v1020-wn-166-119.campus-dynamic.uwaterloo.ca>
2017-03-29 20:15:18 -0400

    prompts for moderator

---
+++ strings.js
@@ -0,0 +1,14 @@
+var prompts ={
+  warzone: {
+    beforeSleeping: 'The army goes to sleep',
+    afterSleeping: ['The batallion were sleeping when they heard a noise. They quickly went outside to investigate it. Meanwhile the one soldier slow of the blocks was stabbed by an assassin ' ,
+     'There was one outspoken, brave soldier amongst us. Unfortunately the cahoots didnt like that and thus he was hit by an archer',
+     ]
+  },
+  general: {
+    beforeSleeping: 'Everyone go to sleep',
+    afterSleeping: 'Everyone Wake up',
+    beforeCahootsVote: 'Cahoots open your eyes',
+    afterCahootsVote: 'Cahoots close your eyes'
+  }
+}
