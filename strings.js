de76cfcb2ac6cc2983ad79c1f07c7e5aeb05c2e3
Saif Mahamood <saifmahamood@v1020-wn-166-119.campus-dynamic.uwaterloo.ca>
2017-03-29 20:22:51 -0400

    added Govt

--- strings.js
+++ strings.js
@@ -1,6 +1,6 @@
 var prompts ={
   warzone: {
-    beforeSleeping: 'The army goes to sleep',
+    beforeSleeping: 'Soldiers, go to sleep!',
     afterSleeping: ['The batallion were sleeping when they heard a noise. They quickly went outside to investigate it. Meanwhile the one soldier slow of the blocks was stabbed by an assassin ' ,
      'There was one outspoken, brave soldier amongst us. Unfortunately the cahoots didnt like that and thus he was hit by an archer',
      ]
@@ -10,5 +10,10 @@ var prompts ={
     afterSleeping: 'Everyone Wake up',
     beforeCahootsVote: 'Cahoots open your eyes',
     afterCahootsVote: 'Cahoots close your eyes'
+  },
+  government: {
+    beforeSleeping: 'Citizens, go to sleep!',
+    afterSleeping: ['There was an individual revolutionary causing protests and disruptions for the govt. Unfortunately the deep state got _____ assassinated',
+    'The government wanted a scapegoat. So they got ____ hung till death under false charges']
   }
 }
